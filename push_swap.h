a2dfbb2f109609fd1aea018c05dd90d79e5e6df6
Nuloiz <nschutz@student.42heilbronn.de>
2026-03-06 15:37:00 +0100

    Fixing Compile Errors in turk_sort.c

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: nschutz <nschutz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 11:35:42 by nschutz           #+#    #+#             */
-/*   Updated: 2026/03/06 14:44:46 by nschutz          ###   ########.fr       */
+/*   Updated: 2026/03/06 15:35:08 by nschutz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,8 @@ typedef struct s_node
 
 typedef struct s_cost
 {
-	int total_cost;
-	struct s_cost *pos;
+	int 			total_cost;
+	struct s_node	*pos;
 }				t_cost;
 
 int		main(int argc, char	**argv);
--- turk_sort.c
+++ turk_sort.c
@@ -6,41 +6,25 @@
 /*   By: nschutz <nschutz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/06 12:08:58 by nschutz           #+#    #+#             */
-/*   Updated: 2026/03/06 14:56:50 by nschutz          ###   ########.fr       */
+/*   Updated: 2026/03/06 15:35:43 by nschutz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void turk_sort (t_node **stack_a)
+static int	count_nodes(t_node *stack_a)
 {
-    t_node	*stack_b;
-    t_cost  cost;
-    t_node  *target_node;
-    t_node  *pos_b;
-	int	    nodes;
+	t_node	*pos;
+	int		count;
 
-	stack_b = NULL;
-    cost->total_cost = 2147483647;
-    cost->pos = 0;
-	nodes = count_nodes(stack_a);
-    for (int i = 0; i < (nodes-3); i++)
-        pb(stack_a, &stack_b);
-    three_arg(*stack_a);
-    while (stack_b != NULL)
-    {
-        while (pos_b != NULL)
-        {
-            target_node = find_target_node(*stack_a, stack_b->value);
-            if (cost->total_cost > calculate_cost(*stack_a, stack_b, target_node, pos_b))
-            {
-                cost->total_cost = calculate_cost(*stack_a, stack_b, target_node, pos_b);
-                cost->pos = pos_b;
-            }
-            pos_b = pos_b->next;
-        }
-        execute_operations(stack_a, stack_b, find_operations(*stack_a, stack_b, cost->pos, target_node));
-    }
+	pos = stack_a;
+	count = 0;
+	while (pos != NULL)
+	{
+		count++;
+		pos = pos->next;
+	}
+	return (count);
 }
 
 t_node *find_target_node(t_node *stack_a, int value)
@@ -59,25 +43,56 @@ t_node *find_target_node(t_node *stack_a, int value)
 
 void execute_operations(t_node *stack_a, t_node *stack_b, char **operations)
 {
-    int i
+    int i;
 
     i = 0;
     while (operations[i] != NULL)
     {
-        if (ft_strcmp(operations[i], "ra") == 0)
+        if (ft_strncmp(operations[i], "ra", 2) == 0)
             rotate_stack(&stack_a, NULL, "ra");
-        else if (ft_strcmp(operations[i], "rb") == 0)
+        else if (ft_strncmp(operations[i], "rb", 2) == 0)
             rotate_stack(NULL, &stack_b, "rb");
-        else if (ft_strcmp(operations[i], "rr") == 0)
+        else if (ft_strncmp(operations[i], "rr", 2) == 0)
             rotate_stack(&stack_a, &stack_b, "rr");
-        else if (ft_strcmp(operations[i], "rra") == 0)
+        else if (ft_strncmp(operations[i], "rra", 3) == 0)
             rotate_stack(&stack_a, NULL, "rra");
-        else if (ft_strcmp(operations[i], "rrb") == 0)
+        else if (ft_strncmp(operations[i], "rrb", 3) == 0)
             rotate_stack(NULL, &stack_b, "rrb");
-        else if (ft_strcmp(operations[i], "rrr") == 0)
+        else if (ft_strncmp(operations[i], "rrr", 3) == 0)
             rotate_stack(&stack_a, &stack_b, "rrr");
         i++;
     }
     pa(&stack_a, &stack_b);
     free_array(operations);
+}
+
+void turk_sort (t_node **stack_a)
+{
+    t_node	*stack_b;
+    t_cost  cost;
+    t_node  *target_node;
+    t_node  *pos_b;
+	int	    nodes;
+
+	stack_b = NULL;
+    cost.total_cost = 2147483647;
+    cost.pos = 0;
+	nodes = count_nodes(*stack_a);
+    for (int i = 0; i < (nodes-3); i++)
+        pb(stack_a, &stack_b);
+    three_arg(*stack_a);
+    while (stack_b != NULL)
+    {
+        while (pos_b != NULL)
+        {
+            target_node = find_target_node(*stack_a, stack_b->value);
+            if (cost.total_cost > calculate_cost(*stack_a, stack_b, target_node, pos_b))
+            {
+                cost.total_cost = calculate_cost(*stack_a, stack_b, target_node, pos_b);
+                cost.pos = pos_b;
+            }
+            pos_b = pos_b->next;
+        }
+        execute_operations(*stack_a, stack_b, find_operations(*stack_a, stack_b, cost.pos, target_node));
+    }
 }
