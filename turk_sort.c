b84143a91ada3a5f17aea4a80eda549aec8e3bbe
Nuloiz <nschutz@student.42heilbronn.de>
2026-03-06 13:31:08 +0100

    adding strdup to prevent mallocleaks

--- turk_sort.c
+++ turk_sort.c
@@ -6,7 +6,7 @@
 /*   By: nschutz <nschutz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/06 12:08:58 by nschutz           #+#    #+#             */
-/*   Updated: 2026/03/06 13:13:37 by nschutz          ###   ########.fr       */
+/*   Updated: 2026/03/06 13:29:29 by nschutz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,7 +59,6 @@ t_node *find_target_node(t_node *stack_a, int value)
 char **find_operations(t_node *stack_a, t_node *stack_b, t_node *pos_b, t_node *target_node)
 {
     char    **operations;
-    char    *operation;
     int     cost_a;
     int     cost_b;
     
@@ -70,25 +69,25 @@ char **find_operations(t_node *stack_a, t_node *stack_b, t_node *pos_b, t_node *
     {
         cost_a = count_nodes(stack_a) - cost_a;
         for (int i = 0; i < cost_a; i++)
-            operations[i] = "rra";
+            operations[i] = ft_strdup("rra");
     } else
     {
         for (int i = 0; i < cost_a; i++)
-            operations[i] = "ra";
+            operations[i] = ft_strdup("ra");
     }
     cost_b = calculate_cost_stacks(stack_b, pos_b);
     if (cost_b > count_nodes(stack_b) / 2)
     {
         cost_b = count_nodes(stack_b) - cost_b;
         for (int i = 0; i < cost_b; i++)
-            operations[cost_a + i] = "rrb";
+            operations[cost_a + i] = ft_strdup("rrb");
     } else
     {
         for (int i = 0; i < cost_b; i++)
-            operations[cost_a + i] = "rb";
+            operations[cost_a + i] = ft_strdup("rb");
     }    
     //check for rr and rrr possibilities and adjust operations accordingly
-    operations[cost_a + cost_b] = "pa";
+    operations[cost_a + cost_b] = ft_strdup("pa");
     return (operations);
 }
 
