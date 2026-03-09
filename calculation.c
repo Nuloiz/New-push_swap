d0d9203f0088c209d50e1da6b26764e014a0fdc8
Nuloiz <nschutz@student.42heilbronn.de>
2026-03-09 13:52:34 +0100

    norminette calculation.c

--- calculation.c
+++ calculation.c
@@ -6,36 +6,36 @@
 /*   By: nschutz <nschutz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/06 12:41:01 by nschutz           #+#    #+#             */
-/*   Updated: 2026/03/09 11:29:24 by nschutz          ###   ########.fr       */
+/*   Updated: 2026/03/09 13:52:00 by nschutz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int cost(t_node *stack_a, t_node *stack_b, t_node *t_node, t_node *pos_b)
+int	cost(t_node *stack_a, t_node *stack_b, t_node *t_node, t_node *pos_b)
 {
-    int cost_a;
-    int cost_b;
+	int	cost_a;
+	int	cost_b;
 
-    cost_a = calculate_cost_stacks(stack_a, t_node);
-    if (cost_a > count_nodes(stack_a) / 2)
-        cost_a = count_nodes(stack_a) - cost_a;
-    cost_b = calculate_cost_stacks(stack_b, pos_b);
-    if (cost_b > count_nodes(stack_b) / 2)
-        cost_b = count_nodes(stack_b) - cost_b;
-    //check for rr and rrr possibilities and adjust costs accordingly
-    return (cost_a + cost_b);
+	cost_a = calculate_cost_stacks(stack_a, t_node);
+	if (cost_a > count_nodes(stack_a) / 2)
+		cost_a = count_nodes(stack_a) - cost_a;
+	cost_b = calculate_cost_stacks(stack_b, pos_b);
+	if (cost_b > count_nodes(stack_b) / 2)
+		cost_b = count_nodes(stack_b) - cost_b;
+	//check for rr and rrr possibilities and adjust costs accordingly
+	return (cost_a + cost_b);
 }
 
-int calculate_cost_stacks(t_node *stack, t_node *target)
+int	calculate_cost_stacks(t_node *stack, t_node *target)
 {
-    int cost;
+	int	cost;
 
-    cost = 0;
-    while (stack != target)
-    {
-        cost++;
-        stack = stack->next;
-    }
-    return (cost);
+	cost = 0;
+	while (stack != target)
+	{
+		cost++;
+		stack = stack->next;
+	}
+	return (cost);
 }
