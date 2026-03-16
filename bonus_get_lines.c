7d0d087413307f7c93488c9271126b1b1e6c16fc
Nuloiz <nschutz@student.42heilbronn.de>
2026-03-16 16:24:57 +0100

    compile error fix

--- bonus_get_lines.c
+++ bonus_get_lines.c
@@ -6,11 +6,34 @@
 /*   By: nschutz <nschutz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/16 12:47:38 by nschutz           #+#    #+#             */
-/*   Updated: 2026/03/16 14:57:46 by nschutz          ###   ########.fr       */
+/*   Updated: 2026/03/16 16:22:45 by nschutz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bonus_checker.h"
+
+int	no_int_twice(char **list)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	i = 0;
+	while (list[i])
+	{
+		j = i;
+		k = (int)ft_atoi(list[i]);
+		while (list[j + 1])
+		{
+			if (k == (int)ft_atoi(list[j + 1]))
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
 
 char	**get_lines(void)
 {
